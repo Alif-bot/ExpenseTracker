6196083e49263ee2d9d1faaac4615d472d52fce2
Md Alif Hossain <infoalif7@gmail.com>
2024-10-23 22:02:34 +0600

    added font weight

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Expense Tracker")
+                .font(.title)
+                .bold()
         }
         .padding()
     }
